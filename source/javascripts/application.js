00806bbec5f71f0dd3940c6df6b7e7761b90c8ee
Sergio Alves <salves2383@gmail.com>
2018-02-06 20:54:30 +0100

    Refactoring on the code, navbar is now more clean, also implementing responsive navbar with javascript

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,30 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+function attachEvents() {
+  window.addEventListener("scroll", runScroll);
+}
+
+
+function runScroll() {
+  const nav = document.querySelector(".navbar-wagon");
+  const links = document.querySelectorAll(".navbar-wagon-link");
+
+   if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100)  {
+     nav.classList.add("nav-scroll");
+     links.forEach(function(item) {
+       item.classList.add("navbar-link-scroll");
+     });
+   } else {
+     nav.classList.remove("nav-scroll");
+     links.forEach(function(item) {
+       item.classList.remove("navbar-link-scroll");
+     });
+   }
+}
+
+
+document.addEventListener("DOMContentLoaded", attachEvents);
+
