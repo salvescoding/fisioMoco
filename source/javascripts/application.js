79f5128ba1cab3b642f7588cebc6fc4d4ac258ec
Sergio Alves <salves2383@gmail.com>
2018-02-07 13:03:18 +0100

    Navbar finished, and homepage new look implemented

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -12,7 +12,7 @@ function runScroll() {
   const nav = document.querySelector(".navbar-wagon");
   const links = document.querySelectorAll('div.navbar-wagon-right > a');
 
-   if (document.body.scrollTop >= 380 || document.documentElement.scrollTop >= 380)  {
+   if (document.body.scrollTop >= 340 || document.documentElement.scrollTop >= 340)  {
      nav.classList.add("nav-scroll");
    } else {
      nav.classList.remove("nav-scroll");
