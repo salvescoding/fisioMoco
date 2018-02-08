3510dd9acdee7ecf8dbae297274aea289ca5acb4
Sergio Alves <salves2383@gmail.com>
2018-02-08 17:36:53 +0100

    commented code

--- source/javascripts/scrollNavBar.js
+++ source/javascripts/scrollNavBar.js
@@ -1,22 +1,22 @@
 function attachEvents() {
   window.addEventListener("scroll", runScroll);
-  init()
+  //init()
 }
 
 
-function init() {
-  let title = document.querySelector(".title");
-  let opacity = 0.1;  // initial opacity
-  let timer = setInterval(fadeIn, 200);
-  function fadeIn(){
-    if (opacity >= 1){
-      clearInterval(timer);
-    } else {
-      title.style.opacity = opacity;
-      opacity += 0.1;
-    }
-  }
-}
+// function init() {
+//   let title = document.querySelector(".title");
+//   let opacity = 0.1;  // initial opacity
+//   let timer = setInterval(fadeIn, 200);
+//   function fadeIn(){
+//     if (opacity >= 1){
+//       clearInterval(timer);
+//     } else {
+//       title.style.opacity = opacity;
+//       opacity += 0.1;
+//     }
+//   }
+// }
 
 function runScroll() {
   const nav = document.querySelector(".navbar-wagon");
