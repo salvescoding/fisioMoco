5b2ca9afe4b4039b056fb56924ad1965ef876438
Sergio Alves <salves2383@gmail.com>
2018-02-08 17:28:35 +0100

    Fixing bugs

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,5 +2,20 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+function init() {
+  let titles = document.querySelector(".title");
+  let opacity = 0.1;  // initial opacity
+  let timer = setInterval(fadeIn, 200);
+  function fadeIn(){
+    if (opacity >= 1){
+      clearInterval(timer);
+    } else {
+      titles.style.opacity = opacity;
+      opacity += 0.1;
+    }
+  }
+}
 
+
+document.addEventListener("DOMContentLoaded", init);
 
