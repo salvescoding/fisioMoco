56a51d85341d1c61e0a801eeb8ae44f748eaa345
Sergio Alves <salves2383@gmail.com>
2018-02-08 21:23:33 +0100

    Items now appear as selected when on the page

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,31 +1,28 @@
 function attachEvents() {
   window.addEventListener("scroll", runScroll);
-  //init();
+  selected();
 }
 
-// function init() {
-//   let title = document.querySelector(".title");
-//   let opacity = 0.1;  // initial opacity
-//   let timer = setInterval(fadeIn, 200);
-//   function fadeIn(){
-//     if (opacity >= 1){
-//       clearInterval(timer);
-//     } else {
-//       title.style.opacity = opacity;
-//       opacity += 0.1;
-//     }
-//   }
-// }
+function selected() {
+  const links = document.querySelectorAll('div.navbar-wagon-right > a');
+  let url = window.location.href;
+  links.forEach(function(element){
+    if(url.includes(element.pathname) === true) {
+      //console.log(element);
+      element.classList.add("selected");
+    }
+  });
+}
 
 function runScroll() {
   const nav = document.querySelector(".navbar-wagon");
-  const links = document.querySelectorAll('div.navbar-wagon-right > a');
 
    if (document.body.scrollTop >= 240 || document.documentElement.scrollTop >= 240)  {
      nav.classList.add("nav-scroll");
    } else {
      nav.classList.remove("nav-scroll");
    }
+
 }
 
 
