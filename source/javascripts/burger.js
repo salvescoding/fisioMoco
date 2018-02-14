4e3281d5e7923ab674828e9a2a3ac9b085ca642c
Sergio Alves <salves2383@gmail.com>
2018-02-14 20:20:44 +0100

    Deleted burger js file

--- source/javascripts/burger.js
+++
@@ -1,7 +0,0 @@
-var burger = document.getElementById("burger-button");
-
-burger.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.classList.toggle("open");
-  burger.classList.toggle("open");
-});
